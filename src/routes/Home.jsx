ebbae1c2e6523d264ea8e140b544e321455ae067
Luan Chiodini <a.luanjc@gmail.com>
2024-06-23 07:59:55 -0300

    Adicionando efeito de digitação

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,3 +1,51 @@
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+
+const Cursor = () => {
+  const [visible, setVisible] = useState(true);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setVisible((prevVisible) => !prevVisible);
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <span>{visible ? "|" : " "}</span>;
+};
+
+const TypingEffect = ({ text }) => {
+  const [displayText, setDisplayText] = useState("");
+  const [isTyping, setIsTyping] = useState(true);
+
+  useEffect(() => {
+    let currentIndex = 0;
+    const interval = setInterval(() => {
+      if (currentIndex <= text.length) {
+        setDisplayText(text.slice(0, currentIndex));
+        currentIndex++;
+      } else {
+        clearInterval(interval);
+        setIsTyping(false);
+      }
+    }, 150);
+
+    return () => clearInterval(interval);
+  }, [text]);
+
+  return (
+    <>
+      <span>{displayText}</span>
+      {isTyping && <Cursor />}{" "}
+    </>
+  );
+};
+
+TypingEffect.propTypes = {
+  text: PropTypes.string.isRequired,
+};
+
 const Home = () => {
   return (
     <main>
@@ -7,7 +55,10 @@ const Home = () => {
             <div className="title-container">
               <p>Hi all. I am</p>
               <h1>Luan Jose Chiodini</h1>
-              <h2>Front-end developer</h2>
+              <h2>
+                <i className="ri-arrow-right-s-line"></i>
+                <TypingEffect text="Front-end developer" />
+              </h2>
             </div>
             <div className="description-container">
               <p>{"// complete the game to continue"}</p>
