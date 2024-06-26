18066685ebe3bb6eecd84064eaba4e591a46efad
Luan Chiodini <a.luanjc@gmail.com>
2024-06-26 17:27:59 -0300

    Arrumandod detalhes no codigo

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./Responsive.css";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
